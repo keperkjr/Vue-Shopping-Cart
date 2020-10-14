db761566ed3dddbeaa0223bae3aa07fc4921c6c6
keperkjr <keperkjr@yahoo.com>
2020-10-13 18:36:11 -0700

    - Added functions to get the cart total - Format the currency with the dollar symbol

--- src/store/index.js
+++ src/store/index.js
@@ -11,11 +11,40 @@ export default new Vuex.Store({
     // {id, quantity}
     cart: [],
   },
+
+
   getters: { // computed properties
+
     availableProducts(state, getters) {
       return state.products.filter(product => product.inventory > 0);
+    },
+
+    cartProducts(state, getters) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(product => product.id == cartItem.id);
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity,
+        };
+      });
+    },
+
+    cartTotal(state, getters) {
+      // let total = 0;
+
+      // getters.cartProducts.forEach(product => {
+      //   total += product.price * product.quantity;
+      // });
+
+      // return total;
+
+      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0);
     }
+
   },
+
+
   actions: { // methods
     fetchProducts(context) {
       return new Promise((resolve, reject) => {
