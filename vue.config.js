b26b4bc12b4b11f69cf7e904837e946d8cd17261
keperkjr <keperkjr@yahoo.com>
2021-01-17 15:37:39 -0800

    Added publicPath in vue.config.js for deployment

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,15 @@
 //    Description: Adds additional options to the site config
 // ============================================================================ 
 module.exports = {
+    // Project deployment base
+    // By default we assume your app will be deployed at the root of a domain,
+    // e.g. https://www.my-app.com/
+    // If your app is deployed at a sub-path, you will need to specify that
+    // sub-path here. For example, if your app is deployed at
+    // https://www.foobar.com/my-app/
+    // then change this to '/my-app/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/demo/vue-store/' : '/',
+   
     chainWebpack: config => {
         config
         .plugin('html')
