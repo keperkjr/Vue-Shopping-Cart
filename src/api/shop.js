6dbf32d4f5ee2deace01619f51ac82c968a6691f
keperkjr <keperkjr@yahoo.com>
2021-01-16 18:28:48 -0800

    Added a default image icon for categories with no image

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,36 +1,41 @@
-/**
- * Mocking client-server processing
- */
+// ============================================================================
+//    Author: Kenneth Perkins
+//    Date:   Oct 13, 2020
+//    Taken From: http://programmingnotes.org/
+//    File:  shop.js
+//    Description: Mocking client-server processing
+// ============================================================================ 
+
 // The images below are stored in the 'assets' folder. You could 
 // also use an image hosted online as the image source
 const _products = [
     {
-        "id": 1, 
-        "title": 
-        "iPad 4 Mini", 
-        "price": 500.01, 
-        "inventory": 2, 
+        'id': 1, 
+        'title': 
+        'iPad 4 Mini', 
+        'price': 500.01, 
+        'inventory': 2, 
         // Source: https://images-na.ssl-images-amazon.com/images/I/61V9XmqGscL._AC_SX679_.jpg
-        "image": 'ipad.jpg',
-        "categories": ["Electronics"],
+        'image': 'ipad.jpg',
+        'categories': ['Electronics'],
     },
     {
-        "id": 2, 
-        "title": "H&M T-Shirt White", 
-        "price": 10.99, 
-        "inventory": 10, 
+        'id': 2, 
+        'title': 'H&M T-Shirt White', 
+        'price': 10.99, 
+        'inventory': 10, 
         // Source: https://i.pinimg.com/474x/ea/ac/ef/eaacef3ef23baa14d93cc51aeb3e2bae.jpg
-        "image": 't-shirt.jpg',
-        "categories": ["Clothing"], 
+        'image': 't-shirt.jpg',
+        'categories': ['Clothing'], 
     },
     {
-        "id": 3, 
-        "title": "Charli XCX - Sucker CD", 
-        "price": 19.99, 
-        "inventory": 5, 
+        'id': 3, 
+        'title': 'Charli XCX - Sucker CD', 
+        'price': 19.99, 
+        'inventory': 5, 
         // Source: https://img.discogs.com/OdNOxMrIDLtuhxG3jAoMuy1sKg4=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6417881-1582920045-4712.jpeg.jpg
-        "image": 'sucker-cd.jpg',
-        "categories": ["Music", "Electronics"]
+        'image': 'sucker-cd.jpg',
+        'categories': ['Music', 'Electronics']
     },
 ]
 
--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -210,6 +210,10 @@ export default {
     background-repeat: no-repeat; 
 }
 
+.icon:before {
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='60' height='60' viewBox='0 0 24 24' fill='none' stroke='%23000000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z'%3E%3C/path%3E%3Cline x1='7' y1='7' x2='7.01' y2='7'%3E%3C/line%3E%3C/svg%3E");
+}
+
 .icon.music:before {
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%23000000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Ccircle cx='5.5' cy='17.5' r='2.5'/%3E%3Ccircle cx='17.5' cy='15.5' r='2.5'/%3E%3Cpath d='M8 17V5l12-2v12'/%3E%3C/svg%3E");        
 }
